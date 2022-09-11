8604fa75403df19688cc1fe4aa713f78115b3442
ggustavs <ggustavsg@gmail.com>
2022-09-11 10:34:33 +0200

    Added contact page and other content
    Added slider to index and fixed button for mobile. Added certificates to media

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,6 @@
+var currentIndex = 0;
+displaySlides(currentIndex);
+
 $(function() {
 	$('.toggler').on('click', function() {
 		$('nav').slideToggle(500);
@@ -22,5 +25,23 @@ function myFunction(dotsn,moren,btnTextn) {
 	  moreText.style.display = "inline";
 	}
   }
-  
+
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("imageSlides");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+		console.log(slides[x])
+    }
+	console.log(currentIndex);
+	let data = [].map.call(slides, slide => slide);
+	console.log(data);
+    slides[currentIndex - 1].style.display = "block";
+}
   
